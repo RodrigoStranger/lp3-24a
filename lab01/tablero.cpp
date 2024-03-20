9061efa3e73cccb63fecf6084c791fe637bdb99b
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-03-20 16:18:32 -0500

    tablero mejorado con apellido en diagonal

--- lab01/tablero.cpp
+++ lab01/tablero.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void ImprimirTablero(int filas, int columnas) {
-    vector<vector<char>> tablero(filas, vector<char>(columnas, ' '));
-    for (int i = 0; i < filas; ++i) {
-        for (int k = 0; k < columnas; ++k) {
-            cout << "----";
-        }
-        cout << "-" << endl;
-        for (int j = 0; j < columnas; ++j) {
-            cout << "| " << tablero[i][j] << " ";
-        }
-        cout << "|" << endl;
+void ImprimirTablero() {
+    char l1 = 'I', l2 = 'N', l3 = 'F', l4 = 'A', l5 = 'N', l6 = 'Z', l7 = 'O', l8 = 'N';
+    char tablero[8][8] = {
+        {l1, ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', l2, ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', l3, ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', l4, ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', l5, ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', l6, ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', l7, ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', l8}
+    };
+    cout << "---------------------------------" << endl;
+    for (int i = 0; i < 8; i++) {
+        cout << "| ";
+    for (int j = 0; j < 8; j++) {
+            cout << tablero[i][j] << " | ";
     }
-    for (int k = 0; k < columnas; ++k) {
-        cout << "----";
-    }
-    cout << "-" << endl;
+        cout << endl << "---------------------------------" << endl;
+}
+
 }
 int main() {
-    ImprimirTablero(10, 10);
+    ImprimirTablero();
     return 0;
 }

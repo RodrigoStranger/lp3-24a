92f2d9454109e8eaa6e5aba08c6af7230b62b0d4
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-03-23 15:00:41 -0500

    Modificando hueso.cpp, agregando funcion

--- lab01/exercises/hueso.cpp
+++ lab01/exercises/hueso.cpp
@@ -2,22 +2,26 @@
 
 using namespace std;
 
+string decisionPerrito(int o1, int t1, int o2, int t2) {
+	if (o1>o2 && t1>t2) {
+		return "Hueso 1";
+	} else if (o1<o2 && t1<t2) {
+		return "Hueso 2";
+	} else {
+		return "Perrito confundido :(";
+	}
+}
+
 int main() {
-    int l1,t1,l2,t2 = 0;
-    cout<<"Digite el olor del hueso 1: ";
-    cin>>l1;
-    cout<<"Digite el tamaÃ±o del hueso 1: ";
-    cin>>t1;
-    cout<<"Digite el olor del hueso 2: ";
-    cin>>l2;
-    cout<<"Digite el tamaÃ±o del hueso 2: ";
-    cin>>t2;
-    if(l1>l2 && t1>t2) {
-        cout<<"Hueso 1";
-    } else if(l1<l2 && t1<t2) {
-        cout<<"Hueso 2";
-    } else {
-        cout<<"Perrito confundido :(";
-    }
-    return 0;
+	int l1, t1, l2, t2;
+	cout<<"Digite el olor del hueso 1: ";
+	cin>>l1;
+	cout<<"Digite el tamaño del hueso 1: ";
+	cin>>t1;
+	cout<<"Digite el olor del hueso 2: ";
+	cin>>l2;
+	cout<<"Digite el tamaño del hueso 2: ";
+	cin>>t2;
+	cout<<"El perrito eligió: "<<decisionPerrito(l1, t1, l2, t2)<<endl;
+	return 0;
 }

974fed604f9da2c2bf5863f758c6ccacd41a0ac6
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-03-23 22:23:13 -0500

    version final

--- lab02/exercises/digitalRootRecursive.cpp
+++ lab02/exercises/digitalRootRecursive.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int DigitalRootRecursivo(int n){
-    if (n < 10) { return n;}
-    int suma = 0;
-    while (n != 0) {
-        suma += n % 10;
-        n /= 10;
-    }
-    return DigitalRoot(suma);
+int DigitalRootRecursive(int n) {
+	if (n < 10) {return n;}
+	
+	int ent = n % 10;
+	int res = n / 10;
+	int suma = ent + res;
+	
+	if (suma > 9) {
+		return DigitalRootRecursive(suma);
+	} else {
+		return suma;
+	}
 }
-
-int main() {
-    cout << DigitalRootRecursivo(154);
-    return 0;
+int main () {
+	int n;
+	cout<<"Ingrese un numero: ";
+	cin>>n;
+	if(n!=0) {
+		cout<<"El numero digital de "<<n<<" es: "<<DigitalRootRecursive(n);
+	}
+	return 0;
 }

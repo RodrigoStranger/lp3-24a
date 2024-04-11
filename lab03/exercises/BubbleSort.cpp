e5b362f12a97b58cb8a83cf54e617d1f7002ba30
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-04-10 23:52:52 -0500

    agregando funciones de imprimir un array e imprimir un contenedor

--- lab03/exercises/BubbleSort.cpp
+++ lab03/exercises/BubbleSort.cpp
@@ -31,6 +31,23 @@ chrono::microseconds MedirTiempo(vector<int>& arr) {
     return duracion;
 }
 
+// Definimos nuestra funcion para imprimir un vector
+void PrintArray(const vector<int>& arr,const int longitud) {
+    for (int i = 0; i < longitud; i++) {
+        cout <<arr[i] << " ";
+    }
+}
+
+// Definimos nuestra funcion para imprimir un vector de vectores
+void PrintArrayContenedor(const vector<vector<int>>& arr,const int longitud) {
+    for (int i = 0; i < longitud; ++i) {
+        cout << i + 1 << ": ";
+        PrintArray(arr[i], arr[i].size());
+        cout<<endl;
+    }
+}
+
+
 int main () {
 
 }

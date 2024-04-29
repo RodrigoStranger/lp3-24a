974b025073d8a7bc4c3c33cf4791b7e137c6e01c
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-04-28 20:48:34 -0600

    agregando correcciones a la funcion principal, estudiando el caso de limitacion de decimales y aproximaciones, optimizando el codigo general, haciendo pruebas unitarias

--- parcial/exercises/trapeciolineal.cpp
+++ parcial/exercises/trapeciolineal.cpp
@@ -1,57 +1,57 @@
 #include <iostream>
+#include <memory>
+#include <iomanip>
 #include <cmath>
 #include <vector>
 #include <fstream>
 
 using namespace std;
 
-double y(double x) {
-    return 2*pow(x,2)+1;
-}
-
 class Trapecio {
     private:
         double altura;
         double basemayor;
         double basemenor;
-        double area = 0;
     public:
         Trapecio(double _altura, double _basemayor, double _basemenor): 
             altura(_altura), basemayor(_basemayor), basemenor(_basemenor) {}
-        ~Trapecio() {}
 
-        double calculararea() {return area=((basemayor+basemenor)*altura)/2;}
+        double calculararea() {return ((basemayor+basemenor)*altura)/2;}
 };
 
+double y(double x) { return 2*pow(x,2)+1;}
+
 // limiteinferior==0 && limitesuperior > 0  || limiteinferior<0 && limitesuperior==0
-double areatotal1(double &limiteinferior, double  &limitesuperior){
-    bool llave =true;
+double areatotal1(double limiteinferior, double limitesuperior, int precision) {
     vector<double> areas;
-    double areatotalverdadera = 0;
-    int c = 1;
-    while(llave) {  
-        double altura = 0;
-        double sum_areas = 0;
-        double temp = 0;
-        altura = (limitesuperior-limiteinferior)/c;
-        for(int i=0;i<c;i++){
-            Trapecio *trapecio = new Trapecio(altura, y(temp+altura), y(temp));
-            temp = temp + altura;
-            sum_areas = sum_areas + trapecio->calculararea();
-            delete trapecio;
+    double areatotalverdadera=0;
+    int c=1;
+    cout<<fixed<<setprecision(precision); // Establecer precisión de salida
+    double tolerancia=pow(10,-precision); // Calcular tolerancia basada en la precisión
+    while(true) {  
+        double altura=0;
+        double sum_areas=0;
+        double temp=0;
+        int c2=1;
+        while(c2<=c) {
+            altura=(limitesuperior-limiteinferior)/c;
+            unique_ptr<Trapecio> trapecio(new Trapecio(altura, y(temp+altura), y(temp)));
+            temp=temp+altura;
+            sum_areas=sum_areas+trapecio->calculararea();
+            c2++;
         }
         areas.push_back(sum_areas);
-        if(areas[c]==areas[c-1]){
-            areatotalverdadera =  areas[c];
-            llave = false;
+        if (areas.size()>1 && abs(areas[c-1]-areas[c-2])<tolerancia) {
+            areatotalverdadera=areas[c-1];
+            return areatotalverdadera;
+            break;
         }    
-    c++;
+        c++;
     }
-    return areatotalverdadera;
 }
 
 // limiteinferior>0 && limitesuperior > 0 || limiteinferior<0 && limitesuperior<0
-double areatotal2(double &limiteinferior, double  &limitesuperior){
+/* double areatotal2(double limiteinferior, double  limitesuperior){
     bool llave =true;
     vector<double> areas;
     double areatotalverdadera = 0;
@@ -69,9 +69,12 @@ double areatotal2(double &limiteinferior, double  &limitesuperior){
     c++;
     }
     return areatotalverdadera;
-}
+} */
 
 int main() {
-    
+    double limitesuperior = 50;
+    double limiteinferior = 0;
+    double precision = 5; 
+    cout << areatotal1(limiteinferior, limitesuperior, precision);
     return 0;
 }

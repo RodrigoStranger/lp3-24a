210e1d588d7154c8dc18243ae5837bc585c37000
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2024-05-01 18:28:18 -0500

    actualizacion menor

---
+++ parcial/exercises/trapecioparalelo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+
+int main () {
+
+    return 0;
+}
